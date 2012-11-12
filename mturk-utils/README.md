888479ec496324f54759da1f84f4b55978983234
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2012-11-12 15:55:48 -0500

    Changes to utils

--- mturk-utils/README.md
+++ mturk-utils/README.md
@@ -3,3 +3,5 @@ All scripts assume that your AWS credentials are in /etc/boto.cfg or ~/.boto fil
     [Credentials]
     aws_access_key_id = <your access key>
     aws_secret_access_key = <your secret key>
+
+The shell scripts require that you have Amazon's [MTurk command line tools](http://mturk.s3.amazonaws.com/CLT_Tutorial/UserGuide.html) installed.  I hope soon to port these to use the Boto package like the python scripts do, but haven't gotten around to it.
--- mturk-utils/run.sh
+++ mturk-utils/run.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
+# print usage information
 if [ $# -lt 1 ]; then
     echo >&2 "usage: $0 prefix [-i inputSuffix -n -sandbox]"    
     exit 1
 fi 
 
+# store current directory to convert local to absolute paths and switch back to at end
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-#cd /Users/dkleinschmidt/code/aws-mturk-clt-1.3.0/bin
+# you must specify the environment variable MTURK_CMD_HOME to point to the
+# directory where the AWS MTurk command lines tools have been installd.
+# see http://mturk.s3.amazonaws.com/CLT_Tutorial/UserGuide.html
 cd $MTURK_CMD_HOME/bin
 
 # process command line flags
 prefix=$1
 shift
 
+# optional suffix for sub-experiments (pilots/other conditions/more subject etc.)
 optsuffix=
+# sandbox flag for filenames
 sandbox=
+# all flags to pass to mturk script.
 additionalFlags=
 
 question=$prefix.question
 
+# parse command line arguments
 while [ $# -gt 0 ]
 do
     case "$1" in
@@ -35,6 +43,7 @@ do
     shift
 done
 
+# timestamp each run to ensure uniqueness
 timestamp=`date +%m-%d-%y-%a-%Hh%Mm%Ss`
 
 # if properties file exists that is specific to optsuffix, use it,
@@ -54,6 +63,7 @@ else
     input=$prefix.input
 fi
 
+# get line from properties files specifying number of assignments to print later
 assignments=`cat $DIR/$properties | grep assignments`
 
 echo "Loading HITs from $DIR"
@@ -69,28 +79,33 @@ echo "Really load these HITs?  Press Enter to continue..."
 read $answer
 echo ""
 
-output=${DIR}/$prefix$optsuffix-$timestamp$sandbox
+# concatenate prefix/timestamp/sandbox label into output filename base
+output=$prefix$optsuffix-$timestamp$sandbox
 
-$dryrun ./loadHITs.sh -label $output -question ${DIR}/$question -properties ${DIR}/$properties -input ${DIR}/$input $additionalFlags $@ > $output.log
+# run the CLT command.
+$dryrun ./loadHITs.sh -label $DIR$output -question $DIR/$question -properties $DIR/$properties -input $DIR/$input $additionalFlags $@ > $DIR/$output.log
 
+cd $DIR
+
+# show the output log.
 cat $output.log
 
 # link the newly created output files to a stable symbolic link (prefix-LATEST.*)
-ln -fs $output.success $DIR/$prefix$sandbox-LATEST.success
-ln -fs $output.log $DIR/$prefix$sandbox-LATEST.log
+ln -fs $output.success $prefix$sandbox-LATEST.success
+ln -fs $output.log $prefix$sandbox-LATEST.log
 
 # concatenate all output/input files together
-if [ ! -f $DIR/$prefix$sandbox-ALL.success ]
+if [ ! -f $prefix$sandbox-ALL.success ]
 then
-    cat $output.success > $DIR/$prefix$sandbox-ALL.success
+    cat $output.success > $prefix$sandbox-ALL.success
 else
-    tail -n+2 $output.success >> $DIR/$prefix$sandbox-ALL.success
+    tail -n+2 $output.success >> $prefix$sandbox-ALL.success
 fi
 
-if [ ! -f $DIR/$prefix$sandbox-ALL.input ]
+if [ ! -f $prefix$sandbox-ALL.input ]
 then
-    cat $DIR/$input > $DIR/$prefix$sandbox-ALL.input
+    cat $input > $prefix$sandbox-ALL.input
 else
-    tail -n+2 $DIR/$intput >> $DIR/$prefix$sandbox-ALL.input
+    tail -n+2 $intput >> $prefix$sandbox-ALL.input
 fi
 
