bac3750b880d489465f956b84e28122f07e8fde3
Dave Kleinschmidt <dkleinschmidt@bcs.rochester.edu>
2014-06-25 14:47:11 -0400

    conditions variables, outline of how to construct lists

--- expt_supunsup.js
+++ expt_supunsup.js
@@ -54,3 +54,58 @@ var stim_images = {
     peak: 'stimuli_images/peak.png'
 };
 
+
+var vwb;
+
+$(document).ready(
+    function() {
+
+        // create an experiment object with the necessary RSRB metadata
+        e = new Experiment(
+            {
+                rsrbProtocolNumber: 'RSRB00045955',
+                rsrbConsentFormURL: 'http://www.hlp.rochester.edu/consent/RSRB45955_Consent_2014-02-10.pdf'
+            }
+        );
+
+        e.init();
+
+        ///////////////////////////////////////////////////////////
+        // parse relevant URL parameters
+        e.sandboxmode = checkSandbox(e.urlparams);
+        e.previewMode = checkPreview(e.urlparams);
+        e.debugMode = checkDebug(e.urlparams);
+        // there are three condition variables:
+        // 1) mean VOTs (10/50 or 30/70; TODO-could)
+        var mean_vots = [10, 50];
+        // 2) up/down shift (TODO)
+        var shift_direction = 'up'
+        var shift = {'up': 10, 'down': -10}[shift_direction];
+        var shifted_mean_vots = _.map(mean_vots, function(vot) { return vot + shift; });
+        // 3) supervised/unsupervised (TODO)
+        var sup_unsup_condition = 'supervised'
+
+        // generate lists
+        var words = ['BEACH', 'BEES', 'BEAK'];
+        // how much of an offset from the category mean VOT for supervised and unsupervised trials
+        var offsets = {'supervised': [-20, 0, 20],
+                       'unsupervised': [-10, 10]};
+        // how many repetitions of each word/VOT?
+        var reps = {'supervised': [1, 17, 1],
+                    'unsupervised': [9, 9]};
+        // images for each word. each value is a list of two image
+        // lists: first one for low VOT (/b/) version, second one for
+        // high VOT (/p/) version of the spoken words
+        var images = {'unsupervised': {'BEACH': [['beach', 'peach'], ['beach', 'peach']],
+                                       'BEES':  [['bees',  'peas'],  ['bees',  'peas']],
+                                       'BEAK':  [['beak',  'peak'],  ['beak',  'peak']]},
+                      'supervised': {'BEACH':   [['beach', 'peas'],  ['beak',  'peach']],
+                                     'BEES':    [['bees',  'peak'],  ['beach', 'peas']],
+                                     'BEAK':    [['beak',  'peach'], ['bees',  'peak']]}
+                     };
+
+        // assemble lists:
+        // iterate over words, and mean VOTs within words, and supervised/unsup within mean VOTs.
+        // for each combination, generate stimuli object, pull out appropriate images, and pull out reps, then put into object.
+        // return list of list item objects.
+    });
