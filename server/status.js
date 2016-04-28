e23568789db7b2bd84a75ca3876e249e8f13f22e
Dave Kleinschmidt <dkleinschmidt@bcs.rochester.edu>
2016-04-28 15:42:08 -0400

    check for status validity

--- server/status.js
+++ server/status.js
@@ -3,19 +3,33 @@
 
 var db = require('./db.js')
   , Assignment = require('./assignment.js')
+  , R = require('ramda')
   ;
+
+var valid_status = ['started', 'finished', 'submitted', 
+                    'returned', 'abandoned',
+                    'accepted', 'rejected'];
+var status_is_valid = R.contains(R.__, valid_status);
 
 module.exports = function(req, res, next) {
     var asgn = Assignment(req.body);
     var new_status = req.params.status;
-    console.log('Request to update status to', new_status, 
-                'received with body', asgn);
 
-    db('assignments')
-        .where(asgn)
-        .returning('status')
-        .update('status', new_status)
-        .then(function(status) {res.send(status);})
-        .catch(next);
+    if (status_is_valid(new_status)) {
     
+        console.log('Request to update status to', new_status, 
+                    'received with body', asgn);
+
+        db('assignments')
+            .where(asgn)
+            .returning('status')
+            .update('status', new_status)
+            .then(function(status) {res.send(status);})
+            .catch(next);
+        
+    } else {
+        next({ error: 'Invalid status:' + new_status,
+               status: new_status });
+    }
+
 };
