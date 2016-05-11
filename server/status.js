2666ca8c50e08940dfc63a76b4cf1b05448799a2
Dave Kleinschmidt <dkleinschmidt@bcs.rochester.edu>
2016-05-11 19:45:02 -0400

    check existing status in db when updating

--- server/status.js
+++ server/status.js
@@ -19,6 +19,23 @@ var valid_status = ['assigned',  // condition assigned
 
 var status_is_valid = R.contains(R.__, valid_status);
 
+var forbidden_prev_status = {
+    "abandoned_before_start": [
+        'started',
+        'finished',
+        'submitted',
+        'accepted',
+        'returned',
+        'abandoned',
+        'rejected'
+    ]
+};
+
+var prev_status_is_valid = function(new_status, old_status) {
+    return R.not(R.contains(old_status,
+                            R.defaultTo([], forbidden_prev_status[new_status])));
+};
+
 module.exports = function(req, res, next) {
     var asgn = Assignment(req.body);
     var new_status = req.params.status;
@@ -26,13 +43,30 @@ module.exports = function(req, res, next) {
     logger.info('Updating status to "%s"', new_status, asgn);
 
     if (status_is_valid(new_status)) {
-        // TODO: check for current status, to prevent updating started+ to
-        // abandoned_before_start
-    
+
         db('assignments')
             .where(asgn)
-            .returning('status')
-            .update('status', new_status)
+            .select('status')
+            .then(R.pipe(R.pluck('status'), R.head))
+            .tap(function(status) {
+                logger.verbose('Current status in db "%s"', status, asgn);
+            })
+            .tap(function(old_status) {
+                if (!prev_status_is_valid(new_status, old_status)) {
+                    throw {
+                        error: 'invalid_status_update',
+                        message: 'Can\'t update ' + old_status + ' to ' + new_status,
+                        status: new_status,
+                        old_status: old_status
+                    };
+                }
+            })
+            .then(function(old_status) {
+                return db('assignments')
+                    .where(asgn)
+                    .returning('status')
+                    .update('status', new_status);
+            })
             .tap(function(status) {
                 logger.verbose('Updated status in db to "%s"',
                                status, asgn);
